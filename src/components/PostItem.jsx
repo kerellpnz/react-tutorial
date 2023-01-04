04a8e005d3a45dd555f44f2ddf8baff5b7dd7c41
k.noskov <k.noskov@lancelot-it.ru>
2023-01-04 19:41:43 +0300

    In progress 1

---
+++ src/components/PostItem.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import MyInput from "./UI/input/MyInput";
+import MyButton from "./UI/button/MyButton";
+
+const PostItem = (props) => {
+    return (
+        <div className="post">
+            <div className="post__content">
+                <strong>{props.number}. {props.post.title}</strong>
+                <div>
+                    {props.post.body}
+                </div>
+            </div>
+            <div className="post_btns">
+                <MyButton>
+                    Delete
+                </MyButton>
+            </div>
+        </div>
+    );
+};
+
+export default PostItem;
